c4a8c71e100dd654e3f71bb5ee69bc4775f05130
M Koivuniemi <mucla@iki.fi>
2022-01-14 00:09:23 +0200

    Make fish move and also kill you

--- app/Main.hs
+++ app/Main.hs
@@ -7,8 +7,13 @@ import Graphics.Gloss.Interface.Pure.Display
 import Lib
 
 type Size     = Float
-type Location = (Float, Float)
 type Velocity = (Float, Float)
+
+width :: (Ord a, Num a) => a
+width = 1500
+
+height :: (Ord a, Num a) => a
+height = 1500
 
 data Me = Me Size Location Velocity -- could also just be a generic Fish
     deriving (Eq, Show)
@@ -39,11 +44,45 @@ drawWorld (Play fishies (Me size (x,y) (vx,vy)))
         me      = color red (pictures [translate x y (circle size)])
         fish    = pictures [translate x y (color orange (circle s)) | Fish s (x,y) _ <- fishies]
 
+simulateLake :: Float -> (Lake -> Lake)
+simulateLake _ GameOver     = GameOver
+simulateLake timeStep (Play fishies me@(Me mySize myPos myVel))
+    | any (collidesWithBiggerFish myPos mySize) fishies = GameOver 
+    | otherwise = Play (map updateFishie fishies)
+                        (Me mySize myNewPos myVel)
+    where 
+        myNewPos :: Location 
+        myNewPos = restoreToScreen (myPos .+ timeStep .* myVel)
+
+        collidesWithBiggerFish :: Location -> Size -> Fish -> Bool
+        collidesWithBiggerFish l s (Fish fs fl _) = (magV (l .- fl) < fs) && (fs > s) 
+
+        updateFishie :: Fish -> Fish
+        updateFishie f@(Fish s l v)
+                = Fish s (restoreToScreen (l .+ timeStep .* v)) v
+
+-- todo: destroy fishie after it's eaten >:)
+{- 
+        updateFishie :: Fish -> Me -> Maybe Fish
+        updateFishie f@(Fish s l v) m@(Me myS _ _)
+            | collidesWith f && s < myS = Nothing
+            | otherwise
+                = Just (Fish (s (restoreToScreen (fp .+ timeStep .* v)) v))
+-}
+       
+restoreToScreen :: Location -> Location
+restoreToScreen (x,y) = (cycleCoordinates x, cycleCoordinates y)
+
+cycleCoordinates :: (Ord a, Num a) => a -> a
+cycleCoordinates x 
+    | x < (-400) = width+x
+    | x > 400    = x-width
+    | otherwise  = x
 
 main :: IO ()
-main = display
-        (InWindow "Drawing our initial world"
-                  (550,550) 
-                  (20,20))
+main = simulate (InWindow "Look at the fishies go!" (width,height) (20,20)) 
         black 
-        (drawWorld initialLake)
+        24 
+        initialLake
+        drawWorld 
+        (\view -> simulateLake)
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,30 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "hello World!"
+type Location = (Float, Float)
+
+(.-) , (.+) :: Location -> Location -> Location
+(x,y) .- (u,v) = (x-u,y-v)
+(x,y) .+ (u,v) = (x+u,y+v)
+
+(.*) :: Float -> Location -> Location
+s .* (u,v) = (s*u,s*v)
+
+infixl 6 .- , .+
+infixl 7 .*
+
+norm :: Location -> Location
+norm (x,y) = let m = magV (x,y) in (x/m,y/m)
+
+magV :: Location -> Float
+magV (x,y) = sqrt (x**2 + y**2) 
+
+limitMag :: Float -> Location -> Location
+limitMag n pt = if (magV pt > n) 
+                  then n .* (norm pt)
+                  else pt
+
+rotateV :: Float -> Location -> Location
+rotateV r (x,y) = (x * cos r - y * sin r
+                  ,x * sin r + y * cos r)
+
+
